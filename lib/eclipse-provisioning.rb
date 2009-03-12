9c3989ef6dd20ec06f72c5bee657793e6465dfb4
Antoine Toulme <antoine@lunar-ocean.com>
2009-03-12 15:59:49 +0100

    added copyright headers to go well with the EPL

--- lib/eclipse-provisioning.rb
+++ lib/eclipse-provisioning.rb
@@ -1,10 +1,15 @@
-# Copyright (c) Intalio, Inc. 2009
+###############################################################################
+# Copyright (c) 2009 Intalio, Inc.
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+#
+# Contributors:
+#     Intalio, Inc. - initial API and implementation
+###############################################################################
 $:.unshift(File.dirname(__FILE__)) unless
   $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
 
 require File.dirname(__FILE__) + '/eclipse-provisioning/bundles_info.rb'
 require File.dirname(__FILE__) + '/eclipse-provisioning/eclipse_instance.rb'
-
-module EclipseProvisioning
-  VERSION = '0.0.1'
-end
